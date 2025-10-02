7acb735d24adbdac0f97c34079374d8467931184
Elsy CHARLES <elsy.s.charles@gmail.com>
2025-10-02 15:08:28 +0300

    dev (refactor) : clean project

--- src/util/routeAlias.util.ts
+++ src/util/routeAlias.util.ts
@@ -1,2 +1,9 @@
 export const ROUTES = {
+    'POST /api/gen?className=model/token.model&action=read&tableName=token': [
+        "/tokens",
+        "/alias/tokens/list"
+    ],
+    'GET /api/meta/fields?className=model/token.model&tableName=token': [
+        "/fields",
+    ],
 };
